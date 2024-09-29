3709d8965f10fef38e2cbc26059a5b3f1f07c179
PEDRO ZAVALA <pedropablozavalat@uc.cl>
2024-09-29 16:16:59 -0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mypage/', // Asegúrate de que esta ruta coincida con el nombre de tu repositorio
+  base: '/mypage', // Asegúrate de que esta ruta coincida con el nombre de tu repositorio
   plugins: [react()],
 })

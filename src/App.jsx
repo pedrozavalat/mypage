8e803b4c5c1436df62c2e11a24457c11fbd1a0f7
PEDRO ZAVALA <pedropablozavalat@uc.cl>
2024-09-29 21:41:08 -0300

    feat: landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Info from './components/Header/Info'
 import './App.css'
+
+
 
 function App() {
   return (
     <>
-      <div>
-        <p>I am just a title</p>
-      </div>
+    <Info />
     </>
   )
 }
---
+++ src/components/Horario/Horario.jsx
@@ -0,0 +1,15 @@
+import './Horario.css';
+
+function Horario() {
+    return (
+        <div className="horario-container">
+            <iframe 
+                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTLWKdmsIG7o2kTfe9pvmQUPB3IypJLG5sKGFKArAwCY2Wnoo2pFK1t3zGGZf_9rBLNLLM5v0mdsm3z/pubhtml?gid=0&single=true" 
+                className="horario"
+            >
+            </iframe>
+        </div>
+    );
+}
+
+export default Horario;
